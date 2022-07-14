9d555058fa65c5ad948b46efc0fcd87bf7c92766
danilZherdev <107792683+danilZherdev@users.noreply.github.com>
2022-07-14 14:13:23 +0300

    add danil

---
+++ WaterShop-main/src/main/java/ru/profiteam/watershop/builder/SellerBuilder.java
@@ -0,0 +1,15 @@
+package ru.profiteam.watershop.builder;
+
+import ru.profiteam.watershop.domain.Seller;
+import ru.profiteam.watershop.domain.User;
+import ru.profiteam.watershop.dto.request.CreateSellerDto;
+import ru.profiteam.watershop.dto.response.SellerDto;
+
+public interface SellerBuilder {
+
+    SellerDto build(Seller seller);
+
+    Seller build(CreateSellerDto request);
+
+    void update(Seller seller, CreateSellerDto request);
+}
